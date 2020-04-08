9a9fccc39d4d110af76d3d9c1a485b3e930f8c26
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-04-08 14:16:22 -0500

    Added displays logi for models and objects details

--- martial_arts/urls.py
+++ martial_arts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.location_list, name='location_list'),
     path('classes/', views.class_schedule_list, name='class_schedule_list'),
+    path('location/<int:pk>', views.location_detail, name='location_detail'),
+    path('classes/<int:pk>', views.class_schedule_detail, name='class_schedule_detail'),
 ]
--- martial_arts/views.py
+++ martial_arts/views.py
@@ -9,3 +9,12 @@ def location_list(request):
 def class_schedule_list(request):
     class_schedules = Class_Schedule.objects.all()
     return render(request, 'martial_arts/class_schedule_list.html', {'class_schedules': class_schedules})
+
+def location_detail(request, pk):
+    location = Location.objects.get(id=pk)
+    return render(request, 'martial_arts/location_detail.html', {'location': location})
+
+def class_schedule_detail(request, pk):
+    class_schedule = Class_Schedule.objects.get(id=pk)
+    return render(request, 'martial_arts/class_schedule_detail.html', {'class_schedule': class_schedule})
+
