959f1a31e8a137bbca3669ff7e89bb9754637c40
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-04-11 04:22:18 -0500

    Trying to fix django module not found error

--- Iowa_Martial_Arts/wsgi.py
+++ Iowa_Martial_Arts/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Iowa_Martial_Arts.settings')
 
 application = get_wsgi_application()
+
+
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
